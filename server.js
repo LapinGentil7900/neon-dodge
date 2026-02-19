b128e540ce1115c936540e29c7f1297c4163f8fe
LapinGentil7900 <lapingentil298@gmail.com>
2026-02-19 17:08:10 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,16 @@
+{
+  "name": "neon-dodge",
+  "version": "3.0.0",
+  "description": "NEON DODGE - Multiplayer arcade game",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js",
+    "dev": "node server.js"
+  },
+  "dependencies": {
+    "express": "^4.18.2"
+  },
+  "engines": {
+    "node": ">=16.0.0"
+  }
+}
